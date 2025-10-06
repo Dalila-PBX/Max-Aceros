79864246f20345952ddd1645bbfd399e76b16fd5
Dalila-PBX <csantiago.pbxhosting@gmail.com>
2025-10-06 09:24:42 -0600

    cambios del 06-10

--- js/main.js
+++ js/main.js
@@ -1,92 +1,124 @@
 /*========SHOW MENU MOBILE==========*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle= document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')  
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*Menu show*/
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*Menu hidden*/
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const bgHeader = () =>{
-    const header = document.getElementById('header')
-    this.scrollY >=50 ? header.classList.add('bg-header')
-                      : header.classList.remove('bg-header')  
-}
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
 
-window.addEventListener('scroll', bgHeader)
-bgHeader()
+window.addEventListener("scroll", bgHeader);
+bgHeader();
 
+/*=============== SWIPER SERVICIO===============*/
+const swiperProductos = new Swiper(".productos__swiper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  centeredSlides: false,
+  freeMode: false,
 
-/*=============== SWIPER SERVICIO===============*/ 
-const swiperProductos = new Swiper('.productos__swiper', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 24,
-    slidesPerView: 'auto',
-
-    navigation: {
-      nextEl: '.swiper-button-next-1',
-      prevEl: '.swiper-button-prev-1',
+  breakpoints: {
+    // Cuando el ancho es >= 1150px
+    1150: {
+      slidesPerView: 3, // Muestra 3 slides en pantallas grandes
+      spaceBetween: 30,
+      centeredSlides: false,
     },
-  });
-
-  /*=============== SWIPER SERVICIO===============*/ 
-const swiperProductos2 = new Swiper('.productos__swiper2', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 24,
-    slidesPerView: 'auto',
-
-    navigation: {
-      nextEl: '.swiper-button-next-2',
-      prevEl: '.swiper-button-prev-2',
+    // Cuando el ancho es >= 1400px
+    1400: {
+      slidesPerView: 4, // Muestra 4 slides en pantallas muy grandes
+      spaceBetween: 40,
+      centeredSlides: false,
     },
-  });
+  },
 
-  /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+  navigation: {
+    nextEl: ".swiper-button-next-1",
+    prevEl: ".swiper-button-prev-1",
+  },
+});
 
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
-    
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 58,
-        sectionId = current.getAttribute('id'),
-        sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+/*=============== SWIPER SERVICIO===============*/
+const swiperProductos2 = new Swiper(".productos__swiper2", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
 
-    if (sectionsClass){
-      if(scrollDown > sectionTop && scrollDown <=      sectionTop + sectionHeight){
-      sectionsClass.classList.add('active-link')
-        
-      }else{
-      sectionsClass.classList.remove('active-link')
-     }     
+  navigation: {
+    nextEl: ".swiper-button-next-2",
+    prevEl: ".swiper-button-prev-2",
+  },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (sectionsClass) {
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass.classList.add("active-link");
+      } else {
+        sectionsClass.classList.remove("active-link");
+      }
     }
-    
-        
-    })
-}
+  });
+};
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
+
+
+/*=============ANIMATION===========*/ 
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "100px",
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.home__content, .productos, .clientes__content`)
+sr.reveal(`.card__clientes2`, {origin:'bottom', delay:1000})
+sr.reveal(`.img-container, .form-container`, {origin: `left`})
+sr.reveal(`.text-container, #categoria .card-base, .card2, .mapa-lado`, {origin: `right`})
+sr.reveal(`#categoria__content2 .card-base`, {interval:100})
+
